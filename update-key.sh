8235f2537f20bf2cbae828d27f088786a421b72b
Andrew Udvare <audvare@gmail.com>
2023-09-16 23:30:55 -0400

    update-key: format

--- update-key.sh
+++ update-key.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 get-key() {
-    while read -r n
-    do
+    while read -r n; do
         if ((n < 0)); then
-            n=$((n + 128 | 128))
+            n=$((n + 128 | 128)) # (n + 128) | 128
         fi
         printf '%x' "$n"
-    done < <(
-        curl -q 'https://raw.githubusercontent.com/dbeaver/dbeaver/devel/plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/BaseProjectImpl.java' 2>/dev/null |
-            grep -F 'LOCAL_KEY_CACHE =' |
-            cut -d= -f2- |
-            cut '-d{' -f2- |
-            sed -r -e 's/\};$//' -e 's/,//g' |
-            perl -lpe 's/^\s+|\s+$//g' |
-            tr ' ' '\n')
+    done < <(curl -q 'https://raw.githubusercontent.com/dbeaver/dbeaver/devel/plugins/org.jkiss.dbeaver.registry/src/org/jkiss/dbeaver/registry/BaseProjectImpl.java' 2> /dev/null |
+        grep -F 'LOCAL_KEY_CACHE =' |
+        cut -d= -f2- |
+        cut '-d{' -f2- |
+        sed -r -e 's/\};$//' -e 's/,//g' |
+        perl -lpe 's/^\s+|\s+$//g' |
+        tr ' ' '\n')
 }
-
-sed -r -e "s/^KEY=.*/KEY=$(get-key)/" -i dbeaver-creds
+sed -re "s/^KEY=.*/KEY=$(get-key)/" -i ./dbeaver-creds
