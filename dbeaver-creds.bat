b7ec4a7000b6b1245b0c5965fa542b6148cb1a0d
Andrew Udvare <audvare@gmail.com>
2023-09-17 02:17:34 -0400

    dbeaver-creds: add error handling

--- dbeaver-creds.bat
+++ dbeaver-creds.bat
@@ -1,4 +1,8 @@
 @echo off
 set KEY=babb4a9f774ab853c96c2d653dfe544a
 set CONFIG_JSON=%APPDATA%\DBeaverData\workspace6\General\.dbeaver\credentials-config.json
-openssl aes-128-cbc -d -K "%KEY%" -iv 00000000000000000000000000000000 -in "%CONFIG_JSON%" | dd bs=1 skip=16 2> nul
+if not exist %CONFIG_JSON% (
+  echo "credentials-config.json not found or could not be read."
+) else (
+  openssl aes-128-cbc -d -K "%KEY%" -iv 00000000000000000000000000000000 -in "%CONFIG_JSON%" | dd bs=1 skip=16 2> nul
+)
