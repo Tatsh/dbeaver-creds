daf9bdc8f87cbea556a8d984485e18d261d6c235
Andrew Udvare <audvare@gmail.com>
2025-07-11 00:42:27 -0400

    Tests and cruft update

---
+++ DBeaverCreds/DBeaverCreds.Tests.ps1
@@ -0,0 +1,55 @@
+$VerbosePreference="Continue"
+BeforeAll {
+  Import-Module -Force "$PSScriptRoot/DBeaverCreds.psm1"
+}
+
+Describe 'DBeaverCreds' {
+  BeforeEach {
+    Mock -ModuleName DBeaverCreds CreateAes { }
+    Mock -ModuleName DBeaverCreds -RemoveParameterType @('JsonData','AesAlg') DecryptData { return "f" }
+    Mock -ModuleName DBeaverCreds -RemoveParameterType @('Bytes') GetString { return "f" }
+  }
+
+  It 'Succeeds' {
+    Mock -ModuleName DBeaverCreds ReadAllBytes { return 'fffffffffffffffffffffffffffffffff' }
+    $data = Show-DBeaver-Credential-Json
+    Should -Invoke -ModuleName DBeaverCreds -CommandName ReadAllBytes -Times 1 -Exactly
+    Should -Invoke -ModuleName DBeaverCreds -CommandName CreateAes -Times 1 -Exactly
+    Should -Invoke -ModuleName DBeaverCreds -CommandName DecryptData -Times 1 -Exactly
+    Should -Invoke -ModuleName DBeaverCreds -CommandName GetString -Times 1 -Exactly `
+      -ParameterFilter { $Index -eq 16 && $Count -eq 17 }
+    $data | Should -Be 'f'
+  }
+
+  It "Fails 'gracefully' when ReadAllBytes throws" {
+    Mock -ModuleName DBeaverCreds ReadAllBytes { throw }
+    $block = { Show-DBeaver-Credential-Json -ErrorAction Stop }
+    $block | Should -Throw
+    Should -Invoke -ModuleName DBeaverCreds -CommandName ReadAllBytes -Times 1 -Exactly
+    Should -Invoke -ModuleName DBeaverCreds -CommandName CreateAes -Times 0 -Exactly
+    Should -Invoke -ModuleName DBeaverCreds -CommandName DecryptData -Times 0 -Exactly
+    Should -Invoke -ModuleName DBeaverCreds -CommandName GetString -Times 0 -Exactly
+  }
+}
+
+Describe 'GetJsonPath' {
+  It 'Returns a Linux Path' {
+    InModuleScope DBeaverCreds {
+      $env:XDG_DATA_HOME = $null
+      GetJsonPath $true $false | Should -Match '\.local/share/'
+    }
+  }
+
+  It 'Returns a Linux path using XDG variable' {
+    InModuleScope DBeaverCreds {
+      $env:XDG_DATA_HOME = '/home/user/.fixed'
+      GetJsonPath $true $false | Should -Match '/home/user/\.fixed'
+    }
+  }
+
+    It 'Returns a macOS path' {
+    InModuleScope DBeaverCreds {
+      GetJsonPath -Linux $false -Mac $true | Should -Match '/Library/'
+    }
+  }
+}
--- DBeaverCreds/DBeaverCreds.psm1
+++ DBeaverCreds/DBeaverCreds.psm1
@@ -1,3 +1,41 @@
+function CreateAes {
+  Param ($Key)
+  $aesAlg = [System.Security.Cryptography.Aes]::Create()
+  $aesAlg.Key = [byte[]] -split ($Key -replace '..', '0x$& ')
+  $aesAlg.IV = [byte[]]::new(16)
+  $aesAlg
+}
+
+function ReadAllBytes {
+  Param ($Path)
+  [System.IO.File]::ReadAllBytes(${Path})
+}
+
+function GetString {
+  Param ([byte[]]$Bytes, [int]$Index, [int]$Count)
+  [System.Text.Encoding]::UTF8.GetString($Bytes, $Index, $Count)
+}
+
+function DecryptData {
+  Param ([byte[]]$JsonData, [System.Security.Cryptography.Aes]$AesAlg)
+  $AesAlg.CreateDecryptor().TransformFinalBlock($JsonData, 0, $JsonData.Length)
+}
+
+function GetJsonPath {
+  Param ([bool]$Linux, [bool]$Mac)
+  $LINUX_MAC_SUFFIX = 'DBeaverData/workspace6/General/.dbeaver/credentials-config.json'
+  if ($Linux) {
+    if ($env:XDG_DATA_HOME) {
+      return "${env:XDG_DATA_HOME}/${LINUX_MAC_SUFFIX}"
+    }
+    return "${env:HOME}/.local/share/${LINUX_MAC_SUFFIX}"
+  }
+  if ($Mac) {
+    return "${env:HOME}/Library/${LINUX_MAC_SUFFIX}"
+  }
+  "${env:APPDATA}\DBeaverData\workspace6\General\.dbeaver\credentials-config.json"
+}
+
 <#
   .SYNOPSIS
   Decrypt and display your DBeaver credentials. The output is a JSON string.
@@ -10,28 +48,16 @@
     dbeaver-creds
 #>
 function Show-DBeaver-Credential-Json {
-  $KEY = 'babb4a9f774ab853c96c2d653dfe544a'
-  $LINUX_MAC_SUFFIX = 'DBeaverData/workspace6/General/.dbeaver/credentials-config.json'
-  $aesAlg = [System.Security.Cryptography.Aes]::Create()
-  $aesAlg.Key = [byte[]] -split ($KEY -replace '..', '0x$& ')
-  $aesAlg.IV = [byte[]]::new(16)
   try {
-    $configJsonData = if ($IsLinux) {
-      [System.IO.File]::ReadAllBytes("${env:HOME}/.local/share/${LINUX_MAC_SUFFIX}")
-    }
-    elseif ($IsMacOS) {
-      [System.IO.File]::ReadAllBytes("${env:HOME}/Library/${LINUX_MAC_SUFFIX}")
-    }
-    else {
-      [System.IO.File]::ReadAllBytes("${env:APPDATA}\DBeaverData\workspace6\General\.dbeaver\credentials-config.json")
-    }
+    $configJsonData = ReadAllBytes -Path (GetJsonPath -Linux $IsLinux -Mac $IsMacOs)
+  } catch {
+    Write-Error -Category ReadError -Message 'credentials-config.json not found or could not be read.'
+    return
   }
-  catch {
-    Write-Error "credentials-config.json not found or could not be read." -Category ReadError
-    Return
-  }
-  $data = $aesAlg.CreateDecryptor().TransformFinalBlock($configJsonData, 0, $configJsonData.Length)
-  [System.Text.Encoding]::UTF8.GetString($data, 16, $data.Length - 16)
+  $KEY = 'babb4a9f774ab853c96c2d653dfe544a'
+  $aesAlg = CreateAes -Key $KEY
+  $data = DecryptData -JsonData $configJsonData -AesAlg $aesAlg
+  GetString -Bytes $data -Index 16 -Count ($data.Length - 16)
 }
 Set-Alias -Name dbeaver-creds -Value Show-DBeaver-Credential-Json
 Export-ModuleMember -Function Show-DBeaver-Credential-Json -Alias dbeaver-creds
